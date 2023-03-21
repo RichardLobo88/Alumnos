9fcf40b5f66b98f8e989f6bd14f031b6e8e05194
RICHARD NAHUEL DEL VALLE LOBO <87398730+RichardLobo88@users.noreply.github.com>
2023-03-21 00:21:40 -0300

    Add files via upload

---
+++ Alumnos/Alumnos/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alumnos
+{
+    internal class Alumno
+    {
+        public int legajo { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string DNI { get; set; }
+        public List<int> Calificacion { get; set; }
+        public void CargarCalificacion(int Nota, DateTime Fecha)
+        {
+            Calificacion.Add(Nota);
+            Console.WriteLine(Fecha.ToString("yyyy") + $" se asigno la calificacion {Nota} al legajo {legajo}");
+        }
+
+        // lo que logra es obtener el promedio de las calificaciones
+        public decimal ObtenerPromedio()
+        {  
+        int acumulador = 0; 
+        foreach (int nota in Calificacion)
+	    {
+            acumulador += nota;
+        }
+            var promedio = acumulador / Calificacion.Count;
+            return promedio;
+	    }
+    }
+    }
+
---
+++ Alumnos/Alumnos/Program.cs
@@ -0,0 +1,26 @@
+﻿using Alumnos;
+
+var alumno1 = new Alumno();
+alumno1.Apellido = "Mateo";
+alumno1.Nombre = "Joaquin";
+alumno1.legajo = 1;
+alumno1.DNI = "30560420";
+alumno1.FechaNacimiento = new DateTime(1986, 11, 4);
+alumno1.Calificacion = new List<int>();
+var alumno2 = new Alumno();
+{
+   alumno2.Apellido = "Lobo";
+    alumno2.Nombre = "Melu";
+    alumno2.legajo = 2;
+    alumno2.DNI = "30562563";
+    alumno2.FechaNacimiento = new DateTime(1988, 11, 4);
+
+
+}
+
+alumno1.CargarCalificacion(6,DateTime.Now);
+alumno1.CargarCalificacion(7, DateTime.Now);
+alumno1.CargarCalificacion(8, DateTime.Now);
+
+
+Console.WriteLine(alumno1.ObtenerPromedio);
